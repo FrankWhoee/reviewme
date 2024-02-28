d299a7ce23cb9123336a6f8ced81752b624d180f
Frank Hui <huifrank@live.com>
2024-02-28 01:05:39 -0800

    Change to tab style

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
-import {Box, ChakraProvider, Divider, Grid, Input, Stack, Textarea, theme} from '@chakra-ui/react';
-import {ColorModeSwitcher} from './ColorModeSwitcher';
-import {RatingSubmit} from "./RatingSubmit";
+import React, { useState } from 'react';
+import { extendTheme, Tab, Tabs, TabList, TabPanel, TabPanels, Box, ChakraProvider, Divider, Grid, Input, Stack, Textarea, theme, Center } from '@chakra-ui/react';
+import { RatingSubmit } from "./RatingSubmit";
 
 function App() {
     let [height, setHeight] = useState(100);
@@ -51,7 +50,7 @@ function App() {
         }
     }
 
-    function handleSubmit(){
+    function handleSubmit() {
         fetch("http://localhost:8000/review", {
             method: "POST",
             body: JSON.stringify({
@@ -66,33 +65,60 @@ function App() {
         });
     }
 
+    // 2. Add your color mode config
+    const config = {
+        initialColorMode: 'dark',
+        useSystemColorMode: true,
+    }
+
+    // 3. extend the theme
+    const theme = extendTheme({ config })
+
     return (
         <ChakraProvider theme={theme}>
-            <Box fontSize="xl" p={4}>
-                <Grid>
-                    <ColorModeSwitcher justifySelf="flex-end"/>
-                </Grid>
-                <Stack>
-                    <Input
-                        placeholder={"What will you write about today?"}
-                        variant={'unstyled'}
-                        size={'xl'}
-                        value={title}
-                        onChange={handleTitleChange}
-                    />
-                    <Divider/>
-                    <Textarea
-                        placeholder={"I give " + (title == "" ? "it" : title.toLowerCase()) + ", 5 stars."}
-                        variant={'unstyled'}
-                        resize={'none'}
-                        height={height}
-                        overflowY={'hidden'}
-                        onChange={handleTextareaChange}
-                        value={text}
-                    />
-                    <RatingSubmit stars={stars} title={title} email={email} onChange={handleEmailChange} onSubmit={handleSubmit}/>
-                </Stack>
-            </Box>
+            <Tabs variant='solid-rounded' colorScheme='gray' isLazy>
+                <Center py={5}>
+                    <TabList>
+                        <Tab mx={1}>review</Tab>
+                        <Tab mx={1}>gallery</Tab>
+                        <Tab mx={1}>profile</Tab>
+                    </TabList>
+                </Center>
+
+                <TabPanels>
+                    <TabPanel>
+                        <Box fontSize="xl" p={4}>
+                            <Stack>
+                                <Input
+                                    placeholder={"What will you write about today?"}
+                                    variant={'unstyled'}
+                                    size={'xl'}
+                                    value={title}
+                                    onChange={handleTitleChange}
+                                />
+                                <Divider />
+                                <Textarea
+                                    placeholder={"I give " + (title == "" ? "it" : title.toLowerCase()) + ", 5 stars."}
+                                    variant={'unstyled'}
+                                    resize={'none'}
+                                    height={height}
+                                    overflowY={'hidden'}
+                                    onChange={handleTextareaChange}
+                                    value={text}
+                                />
+                                <RatingSubmit stars={stars} title={title} email={email} onChange={handleEmailChange} onSubmit={handleSubmit} />
+                            </Stack>
+                        </Box>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>two!</p>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>profile</p>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+
         </ChakraProvider>
     );
 }
